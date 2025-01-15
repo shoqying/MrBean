3f571fa5704d1bfec8a6fc6f445a27c68f1d251f
insung <in27sung@gmail.com>
2025-01-15 19:58:23 +0900

    Update warehouseResultMap

---
+++ MrBean/src/main/java/com/mrbean/warehouse/WarehouseVO.java
@@ -0,0 +1,15 @@
+package com.mrbean.warehouse;
+
+import java.util.Optional;
+
+import lombok.Data;
+
+@Data
+public class WarehouseVO {
+	
+    private String wCode; // 창고 코드
+    private String wName; // 창고 이름
+    private String wAddress; // 창고 주소
+    private Optional<String> wDescription; // 창고 설명
+
+}
