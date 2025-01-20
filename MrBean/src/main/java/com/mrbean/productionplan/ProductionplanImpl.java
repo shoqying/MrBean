4f680d31ce123828e3de1470aae3c2f70283c27b
Insung <110476794+in27sung@users.noreply.github.com>
2025-01-20 17:22:38 +0900

    Update ProductionplanImpl.java

--- MrBean/src/main/java/com/mrbean/productionplan/ProductionplanImpl.java
+++ MrBean/src/main/java/com/mrbean/productionplan/ProductionplanImpl.java
@@ -8,7 +8,6 @@ import javax.inject.Inject;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public class ProductionplanImpl implements ProductionplanDAO {
 
 	@Inject
