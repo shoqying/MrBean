349d52fd1b997e63374900f7cce421728a19e3c3
shoqying <shoqying@gmail.com>
2025-02-19 15:32:08 +0900

    1234

--- MrBean/src/main/java/com/mrbean/finishedproductscontrol/FinishedProductsControlController.java
+++ MrBean/src/main/java/com/mrbean/finishedproductscontrol/FinishedProductsControlController.java
@@ -91,16 +91,15 @@ public class FinishedProductsControlController {
     @ResponseBody
 
     public ResponseEntity<String> updateStatus(@RequestBody FinishedProductsControlVO fvo, RawMaterialsQualityControlVO rvo) {
-        try {            
+        try {
         	finishedProductsControlService.updateStatus(fvo);
         	if (QualityControlStatus.PASS.equals(fvo.getFpcStatus()) || QualityControlStatus.FAIL.equals(fvo.getFpcStatus())) {
-    			finishedProductsControlService.insertFinishedProductLot(rvo);
+        		finishedProductsControlService.insertFinishedProductLot(rvo);
             }
-
-            return ResponseEntity.ok("상태가 업데이트되었습니다.");
-//        } catch (Exception e) {
-//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("업데이트 실패");
-//        }
+        		return ResponseEntity.ok("상태가 업데이트되었습니다.");
+	        } catch (Exception e) {
+	        	return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("업데이트 실패");
+	        }
     }
     
     // 완재품 검사 목록 삭제
